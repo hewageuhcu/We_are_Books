75f28ef869141c01ba37fc7e7ab86eaf7adbf197
U.H. Chathurya Hewage <111672583+hewageuhcu@users.noreply.github.com>
2024-11-28 21:14:44 +0530

    one not working file was removed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Header from './comp/Header';
 import Home from './pages/Home';
-import BookReview from './pages/BookReview';
 import Auth from './pages/Auth';
 
 
@@ -14,7 +13,6 @@ function App() {
         <Header></Header>
         <Routes>
           <Route path="/" element={<Home></Home>} />
-          <Route path="/review" element={<BookReview></BookReview>} />
           <Route path="/auth" element={<Auth></Auth>} />
         </Routes>
     </Router>
